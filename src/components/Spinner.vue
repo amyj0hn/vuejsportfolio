a021e2b3d7c7c6335c9a318a7da0606b8cc97192
amyj0hn <amyj62450@gmail.com>
2024-07-01 09:15:59 +0200

    LandingView

---
+++ src/components/Spinner.vue
@@ -0,0 +1,15 @@
+<template>
+    <div  class="d-flex justify-content-center">
+        <div class="spinner-boarder" role="status"></div>
+        </div>
+    </template>
+    
+    <script>
+    export default {
+    name: "SpinnerComp"
+    }
+    </script>
+    
+    <style>
+    
+    </style>
