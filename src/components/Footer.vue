7a54e072f88a90f7c8bef26dba79cb5f4ec27d72
amyj0hn <amyj62450@gmail.com>
2024-07-01 09:01:28 +0200

    footerContent

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid">
+<div class="row">
+    <p class="lead">Amy's portfolio &copy;
+        <span>{{ cYear }}</span>
+    </p>
+</div> 
+  </div>
+</template>
+
+<script>
+export default {
+name: "FooterComp",
+data(){
+    return {
+        cYear: new Date().
+        getUTCFullYear()
+    }
+}
+ 
+}
+</script>
+
+<style>
+
+</style>
