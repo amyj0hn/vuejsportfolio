d534527ac0ed735d2344f961fc99dc7f356b47d2
amyj0hn <amyj62450@gmail.com>
2024-07-01 07:49:27 +0200

    folderStructure

--- src/main.js
+++ src/main.js
@@ -3,5 +3,11 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+// Bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
+// Bootstrap icons
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App).use(store).use(router).mount('#app')
